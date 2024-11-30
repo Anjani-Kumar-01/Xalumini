4364bbc8517dd4cad37b4afbf816a7824c721633
krishnkant47 <krishnkant979@gmail.com>
2024-12-01 03:11:51 +0530

    add onboarding component

---
+++ frontend/src/components/BlurWindow.jsx
@@ -0,0 +1,9 @@
+const BlurWindow = ({children}) => {
+  return <div className="h-screen sm:bg-[#7F7F80] flex items-center justify-center">
+    <div className="bg-white w-[400px] h-[700px] rounded-md flex flex-col items-center p-5">
+    {children}
+    </div>
+  </div>
+};
+
+export default BlurWindow;
