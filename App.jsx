e88be54d84661164f80f428359a056197c7f8a30
Amnan Singh <amanskh111@gmail.com>
2024-12-11 00:59:24 +0530

    Ashwani Mishra changed this code to puch

--- App.jsx
+++ App.jsx
@@ -1,22 +1,31 @@
-import { Route ,Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import BlurWindow from "./components/BlurWindow";
 import Onboarding1 from "./components/Onboarding1";
 import Onboarding2 from "./components/Onboarding2";
 import Onboarding3 from "./components/Onboarding3";
 import SplashScreen from "./components/SplashScreen";
 import SignIn from "./components/SignIn";
-import home from "./frontend/src/pages/home"
-import signup from "./frontend/src/pages/signup";
-const App = () => {
-	return (
-		<>
-		<Routes>
-   <Route path="./" element={<home/>}/>
-   <Route path="./signin" element={<SignIn/>}/>
-   <Route path="./signup" element={<signup/>}/>
+import Home from "./frontend/src/pages/home";  // Corrected capitalization
+import Signup from "./frontend/src/pages/signup";  // Corrected capitalization
 
-    </Routes>
-		</>
-	)
-}
+const App = () => {
+  return (
+    <>
+      <BlurWindow>
+        {/* <SplashScreen /> */}
+        {/* <Onboarding1 /> */}
+        {/* <Onboarding2 /> */}
+        {/* <Onboarding3 /> */}
+        <SignIn />
+      </BlurWindow>
+
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Corrected path */}
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<Signup />} /> {/* Corrected capitalization */}
+      </Routes>
+    </>
+  );
+};
+
 export default App;
